c9a5eccc3278c084a606851e129a9455d8e44292
fanyukun <fanyukun@new4g.cn>
2019-01-18 21:30:47 +0800

    add discription

--- array/problem34.go
+++ array/problem34.go
@@ -2,6 +2,12 @@ package array
 
 import "fmt"
 
+//Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+//
+//Your algorithm's runtime complexity must be in the order of O(log n).
+//
+//If the target is not found in the array, return [-1, -1].
+
 func searchRange(nums []int, target int) []int {
 	numsLen := len(nums)
 	if numsLen == 0 {
