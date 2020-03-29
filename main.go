ab32b9facf137940ff66ac10b5ee3f973df7e40d
qewetfty <fanyukun10@163.com>
2020-03-30 00:00:46 +0800

    add problem 22, problem 94, problem 144, problem 589, problem 590

--- main.go
+++ main.go
@@ -1,101 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"strconv"
-)
+import "fmt"
 
-// Given an array of strings, group anagrams together.
-//	Example:
-//		Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-//		Output:
-//			[
-//  			["ate","eat","tea"],
-//  			["nat","tan"],
-//  			["bat"]
-//			]
-//	Note:
-//		All inputs will be in lowercase.
-//		The order of your output does not matter.
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//	For example, given n = 3, a solution set is:
+//		[
+//		  "((()))",
+//		  "(()())",
+//		  "(())()",
+//		  "()(())",
+//		  "()()()"
+//		]
 
-// sort string solution
-func groupAnagrams(strs []string) [][]string {
-	if len(strs) < 2 {
-		return [][]string{strs}
-	}
-	resMap := make(map[string][]string)
-	for _, str := range strs {
-		a := byteSlice(str)
-		sort.Sort(a)
-		keyStr := string(a)
-		if v, exist := resMap[keyStr]; exist {
-			v = append(v, str)
-			resMap[keyStr] = v
-		} else {
-			value := make([]string, 0)
-			value = append(value, str)
-			resMap[keyStr] = value
-		}
-	}
-	result := make([][]string, 0)
-	for _, v := range resMap {
-		result = append(result, v)
-	}
-	return result
+func generateParenthesis(n int) []string {
+	res := make([]string, 0)
+	generate(0, 0, n, "", &res)
+	return res
 }
 
-type byteSlice []byte
-
-func (b byteSlice) Len() int {
-	return len(b)
-}
-
-func (b byteSlice) Less(i, j int) bool {
-	return b[i] < b[j]
-}
-
-func (b byteSlice) Swap(i, j int) {
-	b[i], b[j] = b[j], b[i]
-}
-
-// get char count solution
-func groupAnagrams2(strs []string) [][]string {
-	if len(strs) < 2 {
-		return [][]string{strs}
+func generate(left int, right int, n int, s string, res *[]string) {
+	// terminator
+	if left == n && right == n {
+		*res = append(*res, s)
+		return
 	}
-	resMap := make(map[string][]string)
-	for _, str := range strs {
-		intSlice := make([]int, 26)
-		for _, char := range str {
-			intSlice[char-'a']++
-		}
-		intStr := convert2Str(intSlice)
-		if v, exist := resMap[intStr]; exist {
-			v = append(v, str)
-			resMap[intStr] = v
-		} else {
-			value := make([]string, 0)
-			value = append(value, str)
-			resMap[intStr] = value
-		}
-	}
-	result := make([][]string, 0)
-	for _, v := range resMap {
-		result = append(result, v)
-	}
-	return result
-}
 
-func convert2Str(intSlice []int) string {
-	s := ""
-	for _, n := range intSlice {
-		s += strconv.Itoa(n)
+	// process
+	// drill down
+	if left < n {
+		generate(left+1, right, n, s+"(", res)
 	}
-	return s
+	if right < left {
+		generate(left, right+1, n, s+")", res)
+	}
+
+	// reverse states
 }
 
 func main() {
-	a := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
-	fmt.Println(groupAnagrams2(a))
+	fmt.Println(generateParenthesis(3))
+	fmt.Println(generateParenthesis(2))
 }
