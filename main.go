88d04a9fef2d930c1b67e13f855165206071306b
qewetfty <fanyukun10@163.com>
2020-05-17 23:00:46 +0800

    add problem 74

--- main.go
+++ main.go
@@ -2,39 +2,51 @@ package main
 
 import "fmt"
 
-// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
-// (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
-// You are given a target value to search. If found in the array return its index, otherwise return -1.
-// You may assume no duplicate exists in the array.
-// Your algorithm's runtime complexity must be in the order of O(log n).
+// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+//	Integers in each row are sorted from left to right.
+//	The first integer of each row is greater than the last integer of the previous row.
 //	Example 1:
-//		Input: nums = [4,5,6,7,0,1,2], target = 0
-//		Output: 4
+//		Input:
+//			matrix = [
+//			  [1,   3,  5,  7],
+//			  [10, 11, 16, 20],
+//			  [23, 30, 34, 50]
+//			]
+//			target = 3
+//		Output: true
 //	Example 2:
-//		Input: nums = [4,5,6,7,0,1,2], target = 3
-//		Output: -1
+//		Input:
+//			matrix = [
+//			  [1,   3,  5,  7],
+//			  [10, 11, 16, 20],
+//			  [23, 30, 34, 50]
+//			]
+//			target = 13
+//		Output: false
 
-func search(nums []int, target int) int {
-	lo, hi := 0, len(nums)-1
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	height := len(matrix)
+	width := len(matrix[0])
+	lo, hi := 0, height*width-1
 	for lo <= hi {
 		mid := (lo + hi) / 2
-		num := nums[mid]
-		if num == target {
-			return mid
-			// can use xOR to replace
-		} else if (target < num && nums[0] <= target) || (num < nums[0] && target < num) || (num < nums[0] && nums[0] <= target) {
+		x := mid / width
+		y := mid % width
+		num := matrix[x][y]
+		if target == num {
+			return true
+		} else if target < num {
 			hi = mid - 1
 		} else {
 			lo = mid + 1
 		}
 	}
-	return -1
+	return false
 }
 
 func main() {
-	nums := []int{4, 5, 6, 7, 0, 1, 2}
-	fmt.Println(search([]int{5, 1, 3}, 5))
-	fmt.Println(search([]int{1, 3, 5}, 1))
-	fmt.Println(search(nums, 0))
-	fmt.Println(search(nums, 3))
+	fmt.Println(searchMatrix([][]int{{1}}, 2))
 }
