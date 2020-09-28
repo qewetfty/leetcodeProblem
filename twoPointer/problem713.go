b182cc3c503176759cea2b8d1e44ae6268ad62a2
qewetfty <fanyukun10@163.com>
2020-09-28 22:02:42 +0800

    add problem 713

--- twoPointer/problem713.go
+++ twoPointer/problem713.go
@@ -1,6 +1,18 @@
-package main
+package twoPointer
 
 import "fmt"
+
+// Your are given an array of positive integers nums.
+// Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+//	Example 1:
+//		Input: nums = [10, 5, 2, 6], k = 100
+//		Output: 8
+//		Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
+//		Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+//	Note:
+//		0 < nums.length <= 50000.
+//		0 < nums[i] < 1000.
+//		0 <= k < 10^6.
 
 func numSubarrayProductLessThanK(nums []int, k int) int {
 	if k <= 1 {
@@ -40,6 +52,6 @@ func numSubarrayProductLessThanK2(nums []int, k int) int {
 	return res
 }
 
-func main() {
+func testProblem713() {
 	fmt.Println(numSubarrayProductLessThanK([]int{10, 5, 2, 6}, 100))
 }
