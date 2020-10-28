64dddbe93dc951a16e02e577caaa0e23a639281c
qewetfty <fanyukun10@163.com>
2020-10-28 22:44:17 +0800

    add problem 228

--- array/problem228.go
+++ array/problem228.go
@@ -1,4 +1,4 @@
-package main
+package array
 
 import (
 	"fmt"
@@ -69,7 +69,7 @@ func summaryRanges(nums []int) []string {
 	return res
 }
 
-func main() {
+func testProblem228() {
 	fmt.Println(summaryRanges([]int{0, 1, 2, 4, 5, 7}))
 	fmt.Println(summaryRanges([]int{0, 2, 3, 4, 6, 8, 9}))
 	fmt.Println(summaryRanges([]int{}))
