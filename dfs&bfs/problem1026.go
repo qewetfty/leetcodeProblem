02c88d6f733c6c1cd4fab1766aaba6814c84b3df
qewetfty <fanyukun10@163.com>
2020-11-09 23:11:08 +0800

    add problem 1026

--- dfs&bfs/problem1026.go
+++ dfs&bfs/problem1026.go
@@ -1,4 +1,4 @@
-package main
+package dfs_bfs
 
 import (
 	"github.com/leetcodeProblem/data"
@@ -26,36 +26,36 @@ import (
 //		The number of nodes in the tree is in the range [2, 5000].
 //		0 <= Node.val <= 105
 
-var res int
+var res1026 int
 
 // 自上而下记录路径上的最大最小值，当到达叶子节点时候计算最大最小值的差值
 func maxAncestorDiff(root *data.TreeNode) int {
-	res = 0
-	helper(root, root.Val, root.Val)
-	return res
+	res1026 = 0
+	helper1026(root, root.Val, root.Val)
+	return res1026
 }
 
-func helper(node *data.TreeNode, max, min int) {
+func helper1026(node *data.TreeNode, max, min int) {
 	if node == nil {
 		return
 	}
 	max, min = utils.Max(max, node.Val), utils.Min(min, node.Val)
 	// 到达叶子节点，记录路径上最大最小值差值，然后更新result
 	if node.Left == nil && node.Right == nil {
-		res = utils.Max(res, int(math.Abs(float64(max-min))))
+		res1026 = utils.Max(res1026, int(math.Abs(float64(max-min))))
 	}
-	helper(node.Left, max, min)
-	helper(node.Right, max, min)
+	helper1026(node.Left, max, min)
+	helper1026(node.Right, max, min)
 }
 
 // 自下而上记录所有叶子节点的最大最小值，在当前层计算
 func maxAncestorDiff2(root *data.TreeNode) int {
-	res = 0
-	dfs(root)
-	return res
+	res1026 = 0
+	dfs1026(root)
+	return res1026
 }
 
-func dfs(node *data.TreeNode) (int, int) {
+func dfs1026(node *data.TreeNode) (int, int) {
 	// 叶子节点的最大最小值为叶子节点本身
 	if node.Left == nil && node.Right == nil {
 		return node.Val, node.Val
@@ -63,20 +63,16 @@ func dfs(node *data.TreeNode) (int, int) {
 	leftMax, leftMin, rightMax, rightMin := node.Val, node.Val, node.Val, node.Val
 	// 获取左节点的最大最小值
 	if node.Left != nil {
-		newLeftMax, newLeftMin := dfs(node.Left)
+		newLeftMax, newLeftMin := dfs1026(node.Left)
 		leftMax, leftMin = utils.Max(leftMax, newLeftMax), utils.Min(leftMin, newLeftMin)
 	}
 	// 获取右节点的最大最小值
 	if node.Right != nil {
-		newRightMax, newRightMin := dfs(node.Right)
+		newRightMax, newRightMin := dfs1026(node.Right)
 		rightMax, rightMin = utils.Max(rightMax, newRightMax), utils.Min(rightMin, newRightMin)
 	}
 	// 获取当前节点的最大最小值并计算
 	curMax, curMin := utils.Max(leftMax, rightMax), utils.Min(leftMin, rightMin)
-	res = utils.Max(utils.Max(int(math.Abs(float64(node.Val-curMax))), int(math.Abs(float64(node.Val-curMin)))), res)
+	res1026 = utils.Max(utils.Max(int(math.Abs(float64(node.Val-curMax))), int(math.Abs(float64(node.Val-curMin)))), res1026)
 	return curMax, curMin
-}
-
-func main() {
-
 }
