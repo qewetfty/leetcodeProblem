c855efe4407284a7eb9d3f3d37a880bee10286bc
qewetfty <fanyukun10@163.com>
2021-02-14 22:07:37 +0800

    add problem 785

--- dfs&bfs/problem785.go
+++ dfs&bfs/problem785.go
@@ -1,4 +1,4 @@
-package main
+package dfs_bfs
 
 import "fmt"
 
@@ -58,7 +58,7 @@ func isBipartite(graph [][]int) bool {
 	return true
 }
 
-func main() {
+func testProblem785() {
 	fmt.Println(isBipartite([][]int{{1, 3}, {0, 2}, {1, 3}, {0, 2}}))
 	fmt.Println(isBipartite([][]int{{1, 2, 3}, {0, 2}, {0, 1, 3}, {0, 2}}))
 	fmt.Println(isBipartite([][]int{{}, {2, 4, 6}, {1, 4, 8, 9}, {7, 8}, {1, 2, 8, 9}, {6, 9}, {1, 5, 7, 8, 9}, {3, 6, 9}, {2, 3, 4, 6, 9}, {2, 4, 5, 6, 7, 8}}))
