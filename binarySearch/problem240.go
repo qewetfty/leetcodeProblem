64546388b3192ee1c31096fa6c58d9de72ed5229
qewetfty <fanyukun10@163.com>
2021-02-24 09:18:53 +0800

    add problem 240

--- binarySearch/problem240.go
+++ binarySearch/problem240.go
@@ -1,4 +1,4 @@
-package main
+package binarySearch
 
 import "fmt"
 
@@ -21,7 +21,7 @@ import "fmt"
 //		-109 <= target <= 109
 
 // 利用矩阵特性从坐下向右上进行遍历
-func searchMatrix(matrix [][]int, target int) bool {
+func searchMatrix240(matrix [][]int, target int) bool {
 	m, n := len(matrix)-1, 0
 	for m >= 0 && n < len(matrix[0]) {
 		if matrix[m][n] == target {
@@ -58,7 +58,7 @@ func divide(matrix [][]int, startRow, endRow, startCol, endCol, target int) bool
 	}
 }
 
-func main() {
-	fmt.Println(searchMatrix([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5))
-	fmt.Println(searchMatrix([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20))
+func testProblem240() {
+	fmt.Println(searchMatrix240([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5))
+	fmt.Println(searchMatrix240([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20))
 }
