2cfbf33aba35bcd53c9a3a3d697e224a85bd3b97
qewetfty <fanyukun10@163.com>
2021-03-19 15:07:34 +0800

    add problem 376

--- greedy/problem376.go
+++ greedy/problem376.go
@@ -1,4 +1,4 @@
-package main
+package greedy
 
 import (
 	"fmt"
@@ -60,7 +60,7 @@ func helper(nums []int, increase bool) int {
 	return result
 }
 
-func main() {
+func testProblem376() {
 	fmt.Println(wiggleMaxLength([]int{1, 7, 4, 9, 2, 5}))
 	fmt.Println(wiggleMaxLength([]int{1, 17, 5, 10, 13, 15, 10, 5, 16, 8}))
 	fmt.Println(wiggleMaxLength([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}))
