f9bfbfd3eb1fe802cdd57db6796b7f8e64cae5fe
qewetfty <fanyukun10@163.com>
2021-03-24 20:11:43 +0800

    add problem 870

--- greedy/problem870.go
+++ greedy/problem870.go
@@ -1,4 +1,4 @@
-package main
+package greedy
 
 import (
 	"fmt"
@@ -57,7 +57,7 @@ func advantageCount(A []int, B []int) []int {
 	return result
 }
 
-func main() {
+func testProblem870() {
 	fmt.Println(advantageCount([]int{2, 0, 4, 1, 2}, []int{1, 3, 0, 0, 2}))
 	fmt.Println(advantageCount([]int{2, 7, 11, 15}, []int{1, 10, 4, 11}))
 	fmt.Println(advantageCount([]int{12, 24, 8, 32}, []int{13, 25, 32, 11}))
