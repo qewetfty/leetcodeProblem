9cc82873c902c5896bc6111329a7efa2b033a9fc
qewetfty <fanyukun10@163.com>
2021-04-22 23:04:22 +0800

    add problem 554

--- hashTable/problem554.go
+++ hashTable/problem554.go
@@ -1,4 +1,4 @@
-package main
+package hashTable
 
 import (
 	"fmt"
@@ -55,6 +55,6 @@ func leastBricks(wall [][]int) int {
 	return result
 }
 
-func main() {
+func testProblem554() {
 	fmt.Println(leastBricks([][]int{{1, 2, 2, 1}, {3, 1, 2}, {1, 3, 2}, {2, 4}, {3, 1, 2}, {1, 3, 1, 1}}))
 }
