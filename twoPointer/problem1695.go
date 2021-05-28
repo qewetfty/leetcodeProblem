300521ef1d2137f67a131a6c2df20c4d2d64f425
qewetfty <fanyukun10@163.com>
2021-05-28 18:40:02 +0800

    add problem 1695

--- twoPointer/problem1695.go
+++ twoPointer/problem1695.go
@@ -1,4 +1,4 @@
-package main
+package twoPointer
 
 import (
 	"fmt"
@@ -51,7 +51,7 @@ func maximumUniqueSubarray(nums []int) int {
 	return result
 }
 
-func main() {
+func testProblem1695() {
 	fmt.Println(maximumUniqueSubarray([]int{4, 2, 4, 5, 6}))
 	fmt.Println(maximumUniqueSubarray([]int{5, 2, 1, 2, 5, 2, 1, 2, 5}))
 	fmt.Println(maximumUniqueSubarray([]int{187, 470, 25, 436, 538, 809, 441, 167, 477, 110, 275, 133, 666, 345, 411, 459, 490, 266, 987, 965, 429, 166, 809, 340, 467, 318, 125, 165, 809, 610, 31, 585, 970, 306, 42, 189, 169, 743, 78, 810, 70, 382, 367, 490, 787, 670, 476, 278, 775, 673, 299, 19, 893, 817, 971, 458, 409, 886, 434}))
