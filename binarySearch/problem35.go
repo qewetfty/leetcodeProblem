4119202055fe4405f1b6da3517fe4e9ca45aaa55
qewetfty <fanyukun10@163.com>
2021-11-26 08:40:22 +0800

    add problem 35

--- binarySearch/problem35.go
+++ binarySearch/problem35.go
@@ -1,4 +1,4 @@
-package main
+package binarySearch
 
 //Given a sorted array of distinct integers and a target value, return the
 //index if the target is found. If not, return the index where it would be if it were
@@ -52,8 +52,4 @@ func searchInsert(nums []int, target int) int {
 		}
 	}
 	return result
-}
-
-func main() {
-
 }
