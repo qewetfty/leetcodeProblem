dc047125077a720e3c19a1ec200e9fe4116cfac3
qewetfty <fanyukun10@163.com>
2022-09-14 22:20:53 +0800

    edit problem 1457

--- main.go
+++ main.go
@@ -1,56 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"math/big"
-)
+import "leetcodeProblem/data"
 
-func countTexts(pressedKeys string) int {
-	l := len(pressedKeys)
-	char, count := pressedKeys[0], 1
-	result := big.NewInt(1)
-	for i := 1; i < l; i++ {
-		curChar := pressedKeys[i]
-		if curChar == char {
-			count++
-		} else {
-			curCount := getCount(count)
-			result = result.Mul(result, big.NewInt(int64(curCount)))
-			char, count = curChar, 1
-		}
+var result int
+
+func pseudoPalindromicPaths(root *data.TreeNode) int {
+	if root == nil {
+		return 0
 	}
-	curCount := getCount(count)
-	result = result.Mul(result, big.NewInt(int64(curCount)))
-	result = result.Mod(result, big.NewInt(1000000007))
-	return int(result.Int64())
+	result = 0
+	dfs1457(root, 0)
+	return result
 }
 
-func getCount(n int) int64 {
-	if n <= 1 {
-		return 1
+func dfs1457(node *data.TreeNode, temp int) {
+	n := temp ^ (1 << node.Val)
+	if node.Left == nil && node.Right == nil {
+		if n == 0 || n&(n-1) == 0 {
+			result++
+		}
 	}
-	if n == 2 {
-		return 2
+	if node.Left != nil {
+		dfs1457(node.Left, n)
 	}
-	if n == 3 {
-		return 4
+	if node.Right != nil {
+		dfs1457(node.Right, n)
 	}
-	var (
-		x, y, z int64
-	)
-	x, y, z = 1, 2, 4
-	for i := 4; i <= n; i++ {
-		x, y, z = y, z, x+y+z
-	}
-	return z
 }
 
 func main() {
-	fmt.Println(getCount(10000))
-	fmt.Println(countTexts("2"))
-	fmt.Println(countTexts("22"))
-	fmt.Println(countTexts("222"))
-	fmt.Println(countTexts("2222"))
-	fmt.Println(countTexts("22222"))
-	fmt.Println(countTexts("222222"))
+
 }
